c8b7b989bfefbab1a7363afd4fd8e71cf74e81a2
ukrioo <80760888+ukriu@users.noreply.github.com>
2024-08-24 15:25:28 +0530

    Fixes

--- res/script.js
+++ res/script.js
@@ -8,7 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
       l = document.getElementById("twitter-custom-fields"),
       i = document.getElementById("generatedMetaTags"),
       r = document.getElementById("copyButton"),
-      c = document.getElementById("manualColorToggle");
+      c = document.getElementById("manualColorToggle"),
+      ogImage = document.getElementById("ogImage"),
+      ogImageAlt = document.getElementById("ogImageAlt").parentElement;
 
    function d() {
       let e = document.getElementById("charset").value.trim(),
@@ -18,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
          l = document.getElementById("title").value.trim(),
          r = document.getElementById("description").value.trim(),
          d = c.checked ? document.getElementById("themeColorManual").value.trim() : document.getElementById("themeColor").value.trim(),
-         g = document.getElementById("ogUrl").value.trim(),
-         y = document.getElementById("ogImage").value.trim(),
+         g = ogImage.value.trim(),
+         y = g ? document.getElementById("ogImageAlt").value.trim() : "",
          s = a.checked ? document.getElementById("ogTitle").value.trim() : l,
          u = a.checked ? document.getElementById("ogDescription").value.trim() : r,
          E = document.getElementById("twitterCard").value.trim(),
@@ -27,35 +29,38 @@ document.addEventListener("DOMContentLoaded", function () {
          p = o.checked ? document.getElementById("twitterCreator").value.trim() : "",
          I = o.checked ? document.getElementById("twitterImage").value.trim() : "",
          v = `
-<!-- Primary Meta Tags -->
-${e?`<meta http-equiv="CONTENT-TYPE" content="text/html; charset=${e}">`:""}
-${t?`<meta name="viewport" content="${t}">`:""}
-${n?`<meta http-equiv="X-UA-Compatible" content="${n}">`:""}
-${m?`<meta name="robots" content="${m}">`:""}
-${l?`<meta name="title" content="${l}">`:""}
-${r?`<meta name="description" content="${r}">`:""}
-${d?`<meta name="theme-color" content="${d}">`:""}
+<!-- Primary Meta Tags -->\n${e ? `<meta http-equiv="CONTENT-TYPE" content="text/html; charset=${e}">\n` : ""}${t ? `<meta name="viewport" content="${t}">\n` : ""}${n ? `<meta http-equiv="X-UA-Compatible" content="${n}">\n` : ""}${m ? `<meta name="robots" content="${m}">\n` : ""}${l ? `<meta name="title" content="${l}">\n` : ""}${r ? `<meta name="description" content="${r}">\n` : ""}${d ? `<meta name="theme-color" content="${d}">\n` : ""}
+<!-- Open Graph / Facebook -->\n${s ? `<meta property="og:title" content="${s}">\n` : ""}${u ? `<meta property="og:description" content="${u}">\n` : ""}${g ? `<meta property="og:url" content="${g}">\n` : ""}${g ? `<meta property="og:image" content="${g}">\n` : ""}${y ? `<meta property="og:image:alt" content="${y}">\n` : ""}
+<!-- Twitter -->\n${E ? `<meta property="twitter:card" content="${E}">\n` : ""}${h ? `<meta name="twitter:site" content="${h}">\n` : ""}${p ? `<meta name="twitter:creator" content="${p}">\n` : ""}${I ? `<meta name="twitter:image" content="${I}">\n` : ""}`;
 
-<!-- Open Graph / Facebook -->
-${s?`<meta property="og:title" content="${s}">`:""}
-${u?`<meta property="og:description" content="${u}">`:""}
-${g?`<meta property="og:url" content="${g}">`:""}
-${y?`<meta property="og:image" content="${y}">`:""}
-
-<!-- Twitter -->
-${E?`<meta property="twitter:card" content="${E}">`:""}
-${h?`<meta name="twitter:site" content="${h}">`:""}
-${p?`<meta name="twitter:creator" content="${p}">`:""}
-${I?`<meta name="twitter:image" content="${I}">`:""}`;
-      i.value = v.trim()
+      i.value = v.trim();
    }
+
    a.addEventListener("change", function () {
-      t.style.display = this.checked ? "block" : "none", d()
-   }), o.addEventListener("change", function () {
-      l.style.display = this.checked ? "block" : "none", d()
-   }), c.addEventListener("change", function () {
-      n.style.display = this.checked ? "none" : "inline", m.style.display = this.checked ? "inline" : "none", d()
-   }), e.addEventListener("input", d), r.addEventListener("click", function () {
-      i.select(), document.execCommand("copy")
-   })
+      t.style.display = this.checked ? "block" : "none";
+      d();
+   });
+
+   o.addEventListener("change", function () {
+      l.style.display = this.checked ? "block" : "none";
+      d();
+   });
+
+   c.addEventListener("change", function () {
+      n.style.display = this.checked ? "none" : "inline";
+      m.style.display = this.checked ? "inline" : "none";
+      d();
+   });
+
+   ogImage.addEventListener("input", function () {
+      ogImageAlt.style.display = this.value.trim() ? "block" : "none";
+      d();
+   });
+
+   e.addEventListener("input", d);
+
+   r.addEventListener("click", function () {
+      i.select();
+      document.execCommand("copy");
+   });
 });
